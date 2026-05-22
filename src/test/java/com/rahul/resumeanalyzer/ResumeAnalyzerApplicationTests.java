673368ffd2741d6939a8c9c16ebb6be85653a7e5
Rahul Kushwaha <raahullkushwaha@gmail.com>
2026-05-22 18:58:37 +0530

    Add required dependancy

---
+++ src/test/java/com/rahul/resumeanalyzer/ResumeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.resumeanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
